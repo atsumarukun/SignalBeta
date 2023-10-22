4d1cf5bb9cb90f19a2c17f0c0dfa7ec644db1c21
atsumarukun <atys.smz@gmail.com>
2023-10-22 21:30:25 +0900

    ファイルシステムモジュールの作成.

---
+++ uefi/src/fs.rs
@@ -0,0 +1,60 @@
+use alloc::boxed::Box;
+use alloc::vec;
+use uefi::prelude::{Boot, Handle, SystemTable};
+use uefi::proto::media::file::{Directory, File, FileAttribute, FileInfo, FileMode, RegularFile};
+use uefi::CStr16;
+use uefi::Error;
+
+pub fn open_root_dir(
+    image_handle: &Handle,
+    system_table: &SystemTable<Boot>,
+) -> Result<Directory, Error> {
+    system_table
+        .boot_services()
+        .get_image_file_system(*image_handle)?
+        .get_mut()
+        .unwrap()
+        .open_volume()
+}
+
+pub fn open_file(dir: &mut Directory, path: &str) -> Result<SimpleFile, Error> {
+    let mut buf = vec![0; path.len() + 1];
+    Ok(SimpleFile::new(
+        dir.open(
+            &CStr16::from_str_with_buf("hello", &mut buf).unwrap(),
+            FileMode::CreateReadWrite,
+            FileAttribute::empty(),
+        )?
+        .into_regular_file()
+        .unwrap(),
+    ))
+}
+
+pub struct SimpleFile {
+    pub file: RegularFile,
+}
+
+impl SimpleFile {
+    fn new(file: RegularFile) -> Self {
+        Self { file: file }
+    }
+
+    pub fn get_info(&mut self) -> Result<Box<FileInfo>, Error> {
+        self.file.get_boxed_info::<FileInfo>()
+    }
+
+    pub fn read(&mut self) -> Result<vec::Vec<u8>, Error> {
+        let file_size = self.get_info()?.file_size() as usize;
+        let mut buf = vec![0; file_size];
+        self.file.read(&mut buf)?;
+        Ok(buf)
+    }
+
+    pub fn write(&mut self, buffer: &[u8]) -> Result<(), Error<usize>> {
+        self.file.write(buffer)
+    }
+
+    pub fn close(self) {
+        self.file.close()
+    }
+}
--- uefi/src/main.rs
+++ uefi/src/main.rs
@@ -3,56 +3,30 @@
 
 extern crate alloc;
 
-use log::info;
-use alloc::vec;
+mod fs;
+
 use core::str::from_utf8;
-use uefi::prelude::*;
-use uefi::CStr16;
-use uefi::proto::media::file::{File, FileMode, FileAttribute, FileInfo};
+use log::info;
+use uefi::prelude::{entry, Boot, Handle, Status, SystemTable};
+use uefi::proto::media::file::File;
 
 #[entry]
-fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+fn main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
     info!("Hello world!");
 
-    let mut dir = system_table
-        .boot_services()
-        .get_image_file_system(_image_handle)
-        .unwrap()
-        .get_mut()
-        .unwrap()
-        .open_volume()
-        .unwrap();
+    let mut dir = fs::open_root_dir(&image_handle, &system_table).unwrap();
 
-    let mut buf = [0; 6];
-    let mut file = dir
-        .open(
-            &CStr16::from_str_with_buf("hello", &mut buf).unwrap(),
-            FileMode::CreateReadWrite,
-            FileAttribute::empty(),
-        )
-        .unwrap()
-        .into_regular_file()
-        .unwrap();
-
+    let mut file = fs::open_file(&mut dir, "hello").unwrap();
     file.write("Hello World!".as_bytes()).unwrap();
     file.close();
 
-    let mut buf = [0; 6];
-    let mut file = dir
-        .open(
-            &CStr16::from_str_with_buf("hello", &mut buf).unwrap(),
-            FileMode::Read,
-            FileAttribute::empty(),
-        )
-        .unwrap()
-        .into_regular_file()
-        .unwrap();
-    let file_size = file.get_boxed_info::<FileInfo>().unwrap().file_size() as usize;
-    let mut buf = vec![0; file_size];
-    file.read(&mut buf).unwrap();
+    let mut file = fs::open_file(&mut dir, "hello").unwrap();
+    let data = file.read().unwrap();
     file.close();
-    info!("{:?}", from_utf8(&buf).unwrap());
+    info!("{:?}", from_utf8(&data).unwrap());
+
+    dir.close();
 
     loop {}
     Status::SUCCESS
