a50e597b13dd10b28488e1062da6e18ef7fd2a13
atsumarukun <atys.smz@gmail.com>
2023-11-05 19:26:13 +0900

    ブートローダから画面の描画.

--- uefi/src/main.rs
+++ uefi/src/main.rs
@@ -13,7 +13,8 @@ use core::arch::asm;
 use log::info;
 use uefi::prelude::{entry, Boot, Handle, Status, SystemTable};
 use uefi::proto::media::file::File;
-use uefi::table::boot::{AllocateType, MemoryType};
+use uefi::table::boot::{AllocateType, MemoryType, OpenProtocolParams, OpenProtocolAttributes};
+use uefi::proto::console::gop::GraphicsOutput;
 
 type EntryPoint = extern "C" fn();
 
@@ -44,6 +45,24 @@ fn load_kernel(
     Ok(elf.get_entry())
 }
 
+fn draw_display(system_table: &SystemTable<Boot>) -> Result<(), error::Error> {
+    let boot_services = system_table.boot_services();
+    let handle = boot_services.get_handle_for_protocol::<GraphicsOutput>()?;
+    let mut gop = unsafe { boot_services.open_protocol::<GraphicsOutput>(
+        OpenProtocolParams {
+            handle: handle,
+            agent: boot_services.image_handle(),
+            controller: None,
+        },
+        OpenProtocolAttributes::GetProtocol,
+    )? };
+    let frame_buffer = unsafe { core::slice::from_raw_parts_mut(gop.frame_buffer().as_mut_ptr(), gop.frame_buffer().size()) };
+    frame_buffer.iter_mut().for_each(|buf| {
+        *buf = 255;
+    });
+    Ok(())
+}
+
 fn boot(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Result<(), error::Error> {
     uefi_services::init(&mut system_table)?;
     info!("Hello world!");
@@ -51,6 +70,8 @@ fn boot(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Result<(),
     let buf = read_kernel(&image_handle, &system_table)?;
     let entry_point_ptr = load_kernel(&mut system_table, &buf)?;
     let entry_point: EntryPoint = unsafe { core::mem::transmute(entry_point_ptr) };
+
+    let _ = draw_display(&system_table)?;
 
     let _ = system_table.exit_boot_services(MemoryType::LOADER_DATA);
 
